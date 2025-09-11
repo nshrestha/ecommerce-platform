0b72279f57927652ffdc30fd53ecab1a3163404e
NShre <nshrestha@msn.com>
2025-09-11 14:34:35 -0500

    modified Product Controller for implementing with Postgesql db

--- backend/product-service/src/main/java/com/programming/services/controller/ProductController.java
+++ backend/product-service/src/main/java/com/programming/services/controller/ProductController.java
@@ -1,19 +1,9 @@
 package com.programming.services.controller;
 
-import com.programming.services.dto.ProductRequest;
-import com.programming.services.dto.ProductResponse;
-import com.programming.services.model.Product;
 import com.programming.services.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
@@ -22,58 +12,7 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-   // @PreAuthorize("hasRole('ADMIN') or hasRole('INVENTORY_MANAGER')")
-    public void createProduct(@RequestBody ProductRequest productRequest) {
-        productService.createProduct(productRequest);
-    }
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponse> getAllProducts(ProductResponse productResponse) {
-       return   productService.getAllProducts();
-    }
-
-   /* @GetMapping
-    public ResponseEntity<Page<Product>> getAllProducts(Pageable pageable) {
-        return ResponseEntity.ok(productService.getAllProducts(pageable));
-    }*/
-
-   /* @PutMapping("/{id}")
-    //  @PreAuthorize("hasRole('ADMIN') or hasRole('INVENTORY_MANAGER')")
-    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product product) {
-        return ResponseEntity.ok(productService.updateProduct(id, product));
-    }
-
-    @DeleteMapping("/{id}")
-    //  @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
-        productService.deleteProduct(id);
-        return ResponseEntity.noContent().build();
-    }
 
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable String id) {
-        return ResponseEntity.ok(productService.getProductById(id));
-    }
-
-    @GetMapping("/category/{category}")
-    public ResponseEntity<List<Product>> getProductsByCategory(@PathVariable String category) {
-        return ResponseEntity.ok(productService.getProductsByCategory(category));
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<List<Product>> searchProducts(@RequestParam String query) {
-        return ResponseEntity.ok(productService.searchProducts(query));
-    }
-
-
-    @GetMapping("/price-range")
-    public ResponseEntity<List<Product>> getProductsByPriceRange(
-            @RequestParam BigDecimal min,
-            @RequestParam BigDecimal max) {
-        return ResponseEntity.ok(productService.getProductsByPriceRange(min, max));
-    }*/
 }
